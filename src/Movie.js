95fe8f305402b866c4f503db79c0ba62e9765ba3
virtualcharms <sodalitemix@gmail.com>
2020-08-14 13:23:39 -0700

    created a Movie component

--- src/Movie.js
+++ src/Movie.js
@@ -1,2 +1,30 @@
-import React from 'react';
-import 
+import React, { Component } from 'react';
+
+
+// id, title, and image
+
+class Movie extends Component {
+
+    render() {
+        return()
+        <div className="movie-card">
+            <div>
+                <img src={this.props.url} alt="movie image"></img>
+            </div>
+            <div>
+                <p className="movie-title">
+                    {this.props.movie.title}
+                </p>
+            </div>
+        </div>
+    }
+}
+
+
+
+
+
+
+
+
+export default Movie;
