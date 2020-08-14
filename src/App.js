8c4ddafcc09ff5908f92fbbb3e367190a77e43fe
virtualcharms <sodalitemix@gmail.com>
2020-08-14 13:01:59 -0700

    change height, add movie component, adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
+				<h1>Movie App</h1>
 			</header>
+			<div>
+				<p>This is my Movie Body</p>
+				<div className="movie-grid"></div>
+			</div>
 		</div>
 	);
 }
