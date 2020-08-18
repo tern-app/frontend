0b4709d5ffe0b736a6489973df9b6a344c94b70b
virtualcharms <sodalitemix@gmail.com>
2020-08-17 18:56:11 -0700

    build out create component

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -7,6 +7,11 @@ class Create extends Component {
 		super();
 		this.state = {
 			open: false,
+			newTodo: {},
+			title: '',
+			body: '',
+			dueDate: '',
+			priority: '',
 		};
 	}
 
@@ -18,7 +23,35 @@ class Create extends Component {
 		this.setState({ open: false });
 	};
 
+	handleSubmit = (event) => {
+		console.log(event.target.title.value);
+		const newTodo = {
+			title: this.state.title,
+			body: this.state.body,
+			dueDate: this.state.dueDate,
+			priority: this.state.priority,
+		};
+		this.setState({ newTodo: newTodo });
+	};
+
+	handleTitleChange = (event) => {
+		this.setState({ title: event.target.value });
+	};
+
+	handleBodyChange = (event) => {
+		this.setState({ title: event.target.value });
+	};
+
+	handleDueDateChange = (event) => {
+		this.setState({ title: event.target.value });
+	};
+
+	handlePriorityChange = (event) => {
+		this.setState({ title: event.target.value });
+	};
+
 	render() {
+		console.log(this.state.newTodo);
 		return (
 			<div>
 				<button id='openModal' onClick={this.openModal}>
@@ -31,15 +64,32 @@ class Create extends Component {
 								Close
 							</a>
 							<h1>Create a new to-do</h1>
-							<form>
+							<form onSubmit={this.handleSubmit}>
 								<label>Title:</label>
-								<input type='text' name='title' id='title'></input>
+								<input
+									type='text'
+									name='title'
+									id='title'
+									// title={this.state.title}
+									onChange={this.handleTitleChange}></input>
 								<label>Details:</label>
-								<input type='text' name='body' id='body'></input>
+								<input
+									type='text'
+									name='body'
+									id='body'
+									onChange={this.handleBodyChange}></input>
 								<label>Due Date:</label>
-								<input type='text' name='dueDate' id='dueDate'></input>
+								<input
+									type='text'
+									name='dueDate'
+									id='dueDate'
+									onChange={this.handleDueDateChange}></input>
 								<label>Priority:</label>
-								<input type='text' name='priority' id='priority'></input>
+								<input
+									type='text'
+									name='priority'
+									id='priority'
+									onChange={this.handlePriorityChange}></input>
 								<input type='submit' />
 							</form>
 						</div>
